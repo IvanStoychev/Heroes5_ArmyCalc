0565b66937ce12d50526ddf50ba745d509bad725
Ivan Stoychev <ivanstoychev44@gmail.com>
2017-08-09 20:23:58 +0300

    Committing "Faction" and "Unit" class skeletons.

---
+++ Faction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes5_ArmyCalc
+{
+    class Faction
+    {
+        public string Name { get; set; }
+        //dict or array for units?
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes5_ArmyCalc
+{
+    class Unit
+    {
+        public string Name { get; set; }
+        public byte[] ImgBytes { get; set; }
+        public int BaseGoldCost { get; set; }
+        public int UpgGoldCost { get; set; }
+        public int Population { get; set; }
+    }
+}
