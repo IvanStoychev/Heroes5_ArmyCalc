f94711d3a8b5d7c6655df19c7897f42021a19560
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-07-20 23:42:22 +0300

    Added summaries to "Faction" and "Tier" classes.

--- Faction.cs
+++ Faction.cs
@@ -8,9 +8,21 @@ namespace Heroes5_ArmyCalc
 {
     class Faction
     {
+        /// <summary>
+        /// Tje name of the faction in the game.
+        /// </summary>
         public string Name;
+        /// <summary>
+        /// An array representing information about each tier of this faction.
+        /// </summary>
         public Tier[] Tiers = new Tier[8];
-        public int Dwelling1;
-        public int Dwelling2;
+        /// <summary>
+        /// The tier which's population the faction's first bonus dwelling boosts.
+        /// </summary>
+        public int Dwelling1_Tier;
+        /// <summary>
+        /// The tier which's population the faction's second bonus dwelling boosts.
+        /// </summary>
+        public int Dwelling2_Tier;
     }
 }
--- Tier.cs
+++ Tier.cs
@@ -8,8 +8,21 @@ namespace Heroes5_ArmyCalc
 {
     class Tier
     {
+        /// <summary>
+        /// Denotes which tier the object is for.
+        /// </summary>
+        public int Number;
+        /// <summary>
+        /// The gold cost for an unupgraded creature of this tier.
+        /// </summary>
         public int BaseGoldCost;
+        /// <summary>
+        /// The gold cost for an upgraded creature of this tier.
+        /// </summary>
         public int UpgradedGoldCost;
+        /// <summary>
+        /// This tier's weekly growth.
+        /// </summary>
         public int WeeklyPopulation;
     }
 }
