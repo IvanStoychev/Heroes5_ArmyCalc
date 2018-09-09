7de84b538569413e6ebbbc76e1e27bf9f6e6ba1e
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-09-09 18:30:19 +0300

    Removed "Faction" and "Tier" classes. Added a "Unit" class.

---
+++ Unit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes5_ArmyCalc
+{
+    class Unit
+    {
+        int ID;
+        string NameBase;
+        string NameLeft;
+        string NamRight;
+        string Faction;
+        int Tier;
+        string Description;
+        string ImageBase;
+        string ImageLeft;
+        string ImageRight;
+        int GoldCostBase;
+        int GoldCostUpg;
+        int PopulationBase;
+    }
+}
